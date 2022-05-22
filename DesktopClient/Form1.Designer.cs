cc7e28e99890e3575bef8e05af80d52b274462e6
Janet.Bokovnya <jannet.bokovnya@gmail.com>
2022-05-23 00:28:58 +0300

    create Client desktop

--- DesktopClient/Form1.Designer.cs
+++ DesktopClient/Form1.Designer.cs
@@ -29,35 +29,136 @@ namespace DesktopClient
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.stateLabel = new System.Windows.Forms.Label();
+            this.stateLabelValue = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.getNameButton = new System.Windows.Forms.Button();
+            this.setNameButton = new System.Windows.Forms.Button();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextLabel = new System.Windows.Forms.Label();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.chatTextBox = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // stateLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "State";
+            this.stateLabel.AutoSize = true;
+            this.stateLabel.Location = new System.Drawing.Point(12, 16);
+            this.stateLabel.Name = "stateLabel";
+            this.stateLabel.Size = new System.Drawing.Size(36, 15);
+            this.stateLabel.TabIndex = 0;
+            this.stateLabel.Text = "State:";
             // 
-            // label2
+            // stateLabelValue
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(67, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.stateLabelValue.AutoSize = true;
+            this.stateLabelValue.ForeColor = System.Drawing.Color.Red;
+            this.stateLabelValue.Location = new System.Drawing.Point(56, 16);
+            this.stateLabelValue.Name = "stateLabelValue";
+            this.stateLabelValue.Size = new System.Drawing.Size(79, 15);
+            this.stateLabelValue.TabIndex = 1;
+            this.stateLabelValue.Text = "Disconnected";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(151, 12);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(75, 23);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(12, 45);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(39, 15);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(56, 41);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(170, 23);
+            this.nameTextBox.TabIndex = 3;
+            this.nameTextBox.Text = "Anonymous";
+            // 
+            // getNameButton
+            // 
+            this.getNameButton.Location = new System.Drawing.Point(70, 70);
+            this.getNameButton.Name = "getNameButton";
+            this.getNameButton.Size = new System.Drawing.Size(75, 23);
+            this.getNameButton.TabIndex = 4;
+            this.getNameButton.Text = "Get";
+            this.getNameButton.UseVisualStyleBackColor = true;
+            this.getNameButton.Click += new System.EventHandler(this.getNameButton_Click);
+            // 
+            // setNameButton
+            // 
+            this.setNameButton.Location = new System.Drawing.Point(151, 70);
+            this.setNameButton.Name = "setNameButton";
+            this.setNameButton.Size = new System.Drawing.Size(75, 23);
+            this.setNameButton.TabIndex = 5;
+            this.setNameButton.Text = "Set";
+            this.setNameButton.UseVisualStyleBackColor = true;
+            this.setNameButton.Click += new System.EventHandler(this.setNameButton_Click);
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.Location = new System.Drawing.Point(56, 99);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(170, 77);
+            this.messageTextBox.TabIndex = 6;
+            // 
+            // messageTextLabel
+            // 
+            this.messageTextLabel.AutoSize = true;
+            this.messageTextLabel.Location = new System.Drawing.Point(9, 108);
+            this.messageTextLabel.Name = "messageTextLabel";
+            this.messageTextLabel.Size = new System.Drawing.Size(28, 15);
+            this.messageTextLabel.TabIndex = 7;
+            this.messageTextLabel.Text = "Text";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(151, 182);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 23);
+            this.sendButton.TabIndex = 8;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Location = new System.Drawing.Point(242, 13);
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(546, 425);
+            this.chatTextBox.TabIndex = 9;
+            this.chatTextBox.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.messageTextLabel);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.setNameButton);
+            this.Controls.Add(this.getNameButton);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.stateLabelValue);
+            this.Controls.Add(this.stateLabel);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -67,8 +168,17 @@ namespace DesktopClient
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label stateLabel;
+        private System.Windows.Forms.Label stateLabelValue;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Button getNameButton;
+        private System.Windows.Forms.Button setNameButton;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Label messageTextLabel;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.RichTextBox chatTextBox;
     }
 }
 
--- DesktopClient/Form1.cs
+++ DesktopClient/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,161 @@ namespace DesktopClient
 {
     public partial class Form1 : Form
     {
+        private readonly HubConnection hubConnection;
         public Form1()
         {
             InitializeComponent();
+
+            //создаем hubConnection
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:15980/messages")
+                .WithAutomaticReconnect()
+                .Build();
+            //настраиваем hubConnection, назначаем ему обработчик метода Send
+            //будет приниматься newMessage
+            //получаем сообщение и выводим его в textBox
+            //принимает 3 параметра отправитьель, техст сообщения и цвет сообщения
+            hubConnection.On<NewMessage>("Send", message =>
+            {
+                AppendTextToTextBox(message.Sender, message.Text, Color.Black);
+            });
+            //подптсываемся на 3 события жизненного цикла подключения
+            //1. закрытие 2. событие успешное подключение 3.событие попытки переподключения reconnecting
+            //во всех случаях мы выводим информацию с помощью messagebox о том что событие произошло
+            //событие closed передает объект ошибки
+            //событие reconnected передает connection id, с которым мы переподключились к серверу
+            //событие reconecting передает объект ошибки, почему происходит переподключение
+
+            hubConnection.Closed += error =>
+            {
+                MessageBox.Show($"Connection closed.{error.Message}");
+                return Task.CompletedTask;
+            };
+
+            hubConnection.Reconnected += id =>
+            {
+                MessageBox.Show($"Connection reconnected with id:{id}");
+                return Task.CompletedTask;
+            };
+
+            hubConnection.Reconnecting += error =>
+            {
+                MessageBox.Show($"Connection reconnecting.{error.Message} ");
+                return Task.CompletedTask;
+            };
+
         }
 
+        private void AppendTextToTextBox(string sender, string text, Color color)
+        {
+            chatTextBox.SelectionStart = chatTextBox.TextLength;
+            chatTextBox.SelectionLength = 0;
+            chatTextBox.SelectionColor = color;
+            chatTextBox.AppendText(string.Format("Author:{0}{2}Text:{1}{2}{2}", sender, text, Environment.NewLine));
+            chatTextBox.SelectionColor = chatTextBox.ForeColor;
+        }
+
+        //по кнопке Connect проверяем если наше соединение Disconected, вызываем startasinc для инициализации
+        //подключения, все обернули в try catch и если выпало исключение то программа не закроется аварийно
+        //а выведется ошибка
+       
+        private async void connectButton_Click(object sender, EventArgs e)
+        {
+            if (hubConnection.State == HubConnectionState.Disconnected)
+            {
+                try
+                {
+                    await hubConnection.StartAsync();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                if (hubConnection.State == HubConnectionState.Connected)
+                {
+                    connectButton.Text = "Disconnect";
+                    stateLabelValue.ForeColor = Color.Green;
+                    stateLabelValue.Text = "Connected";
+                }
+            }
+            else if (hubConnection.State == HubConnectionState.Connected)
+            {
+                await hubConnection.StopAsync();
+                connectButton.Text = "Connect";
+                stateLabelValue.ForeColor = Color.Red;
+                stateLabelValue.Text = "Disconnected";
+            }
+        }
+
+        //в обработчике кнопки мы смлтрим если состояние нашего соединения открыто
+        //мы пытаемся получить с сервера наше имя для этого вызываем InvokeAsync дженерик версию и 
+        //указываем что ответ сервера будет типа стринг имя на сервере getmyname
+        private async void getNameButton_Click(object sender, EventArgs e)
+        {
+            if (hubConnection.State == HubConnectionState.Connected)
+            {
+                try
+                {
+                    var name = await hubConnection.InvokeAsync<string>("GetMyName");
+                    if (string.IsNullOrWhiteSpace(name))
+                        nameTextBox.Text = name;
+                    else
+                        nameTextBox.Text = name;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        //при нажатии на кнопку set обработчик проверит открыто ли подключение и попытается передать наше новое имя
+        //с помощью метода sendAsync метод на сервере setmyname
+        private async void setNameButton_Click(object sender, EventArgs e)
+        {
+            if (hubConnection.State == HubConnectionState.Connected)
+            {
+                try
+                {
+                    await hubConnection.SendAsync("SetMyName", nameTextBox.Text);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        //обработчик кнопки send смотрим открыто ли соединениеи создается новый объект типа message
+        //в поле text записывается содержимое нашего messagebox
+        //и пытается отправить на сервер с помощью метода sendtoOthers наше сообщение
+        //и в любом случае удачно или нет отправлено сообщение очищаем messageTextBox
+        private async void sendButton_Click(object sender, EventArgs e)
+        {
+            if (hubConnection.State == HubConnectionState.Connected)
+            {
+                var message = new Message
+                {
+                    Text = messageTextBox.Text
+                };
+                try
+                {
+                    await hubConnection.SendAsync("SendToOthers", message);
+                    AppendTextToTextBox("Me", message.Text, Color.Green);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    messageTextBox.Clear();
+                }
+                
+                
+            
+            }
+        }
     }
 }
