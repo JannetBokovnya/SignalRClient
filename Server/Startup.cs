abb075f662dc783fa78c8a544d8367febcc000eb
Janet.Bokovnya <jannet.bokovnya@gmail.com>
2022-05-24 23:27:50 +0300

    js client

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,13 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Server.Hubs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Server
 {
@@ -18,6 +18,11 @@ namespace Server
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
+
+            //используем технику, которая называется CORS (Cross Origin Resource Sharing).
+            //выполняем запросы из приложения с одного адреса(или домена) к приложению, которое размещено по другому адресу
+            //Для подключения сервисов CORS в приложение в методе ConfigureServices вызывается метод services.AddCors().
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +34,18 @@ namespace Server
             }
 
             app.UseRouting();
+            //Чтобы задействовать CORS для обработки запроса в методе Configure вызывается метод UseCors().
+            //Для настройки конфигурации этот метод использует делегат, в который передается объект CorsPolicyBuilder. 
+            //И с помощью этого объекта можно выполнить настройку CORS.
+            //С помощью метода AllowAnyOrigin() мы указываем, что приложение может обрабатывать запросы от приложений по любым адресам.
+            app.UseCors(policy =>
+            {
+                policy
+                    .SetIsOriginAllowed(origin => true)
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
 
             app.UseEndpoints(endpoints =>
             {
